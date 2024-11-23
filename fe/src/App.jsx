03818bcedffaadc23d5bff87e8e1fc0deec5455d
q <167793812+0vp@users.noreply.github.com>
2024-11-22 21:09:28 -0500

    opn and close mouth hook

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
 import Spline from '@splinetool/react-spline';
-
-import './App.css'
+import './App.css';
 
 function App() {
+    const simulateKeyPress = (key) => {
+        const keyDownEvent = new KeyboardEvent('keydown', { key });
+        document.dispatchEvent(keyDownEvent);
 
-  return (
-    <>
-      <Spline scene="https://prod.spline.design/DvEjptebRZFHNICp/scene.splinecode" />
+        console.log('Key down:', key);
 
-    </>
-  )
+        setTimeout(() => {
+            const keyUpEvent = new KeyboardEvent('keyup', { key });
+            document.dispatchEvent(keyUpEvent);
+            console.log('Key up:', key);
+        }, 1);
+    };
+
+    const handleButtonClick = () => {
+        simulateKeyPress('9');
+    };
+
+    return (
+        <>
+            <button onClick={handleButtonClick}>Press 9</button>
+            <Spline scene="https://prod.spline.design/DvEjptebRZFHNICp/scene.splinecode" />
+        </>
+    );
 }
 
-export default App
+export default App;
