35f4e0c13e4289e49c73f2ea59e8e3e3b85dfb01
ryushen-tan <ryushen@gmail.com>
2024-11-22 20:56:23 -0500

    feat: updated spline model

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Spline from '@splinetool/react-spline';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <Spline scene="https://prod.spline.design/7wrBj9pRjOp01LZn/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/DvEjptebRZFHNICp/scene.splinecode" />
 
     </>
   )
