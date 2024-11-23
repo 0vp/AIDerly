91b21c924463ec2ec9fe0f21158c37a9bf4d0cc6
ryushen-tan <ryushen@gmail.com>
2024-11-23 00:01:52 -0500

    Update Home.jsx

--- fe/src/Home.jsx
+++ fe/src/Home.jsx
@@ -1,6 +1,10 @@
 import Spline from '@splinetool/react-spline';
-
+import Speech from "./utils/Speech";
+import {useState, useEffect} from "react";
 export default function Home() {
+    const [decibel, setDecibel] = useState(0);
+
+
     const simulateKeyPress = (key) => {
         const keyDownEvent = new KeyboardEvent('keydown', { key });
         document.dispatchEvent(keyDownEvent);
@@ -14,14 +18,33 @@ export default function Home() {
         }, 1);
     };
 
+    const handleDecibleLevel = (decibel) => {
+        if (decibel >= 25) {
+            simulateKeyPress('1');
+            simulateKeyPress('4');
+        }
+        else{
+            simulateKeyPress('1');
+            simulateKeyPress('0');
+        }
+    };
+
+    useEffect(() => {
+        handleDecibleLevel(decibel);
+    }, [decibel]);
+    
     const handleButtonClick = () => {
-        simulateKeyPress('9');
+        simulateKeyPress('1');
     };
 
     return (
         <>
             <button onClick={handleButtonClick}>Press 9</button>
             <Spline scene="https://prod.spline.design/DvEjptebRZFHNICp/scene.splinecode" />
+            <Speech toSay={"hi darling"} setDecibel={setDecibel} />
+            <div>
+                {decibel.toFixed(2)} dB
+            </div>
         </>
     );
 }
