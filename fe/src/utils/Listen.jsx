482f3b6b8bd7e9d2ef4a30eeba120ad40348b23e
q <167793812+0vp@users.noreply.github.com>
2024-11-23 03:11:04 -0500

    pauses + voice command

--- fe/src/utils/Listen.jsx
+++ fe/src/utils/Listen.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import "regenerator-runtime/runtime.js";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
@@ -11,23 +11,44 @@ const Listen = () => {
     } = useSpeechRecognition();
 
     const startListening = () => {
-        SpeechRecognition.startListening({ continuous: true });
+        // SpeechRecognition.startListening({ continuous: true });
+        SpeechRecognition.startListening();
     }
 
     const stopListening = () => {
         SpeechRecognition.stopListening();
     }
 
+    const handlePause = () => {
+        // call response on transcript, then reset transcript, then unpause
+        if (transcript.includes('picture of')) {
+            console.log('Taking picture... of', transcript.split('picture of')[1]);
+        }
+    }
+
     if (!browserSupportsSpeechRecognition) {
         return <span>Browser doesn't support speech recognition.</span>;
     }
 
+    useEffect(() => {
+        if (!listening) {
+            // process commands, and or call response
+            console.log('Listening stopped, processing!');
+            handlePause();
+            startListening();
+        }
+    }, [listening]);
+
+    useEffect(() => {
+        startListening();
+    }, []);
+
     return (
         <div>
-            <p>Microphone: {listening ? 'on' : 'off'}</p>
-            <button onClick={startListening}>Start</button>
+            {/* <p>Microphone: {listening ? 'on' : 'off'}</p> */}
+            {/* <button onClick={startListening}>Start</button>
             <button onClick={stopListening}>Stop</button>
-            <button onClick={resetTranscript}>Reset</button>
+            <button onClick={resetTranscript}>Reset</button> */}
             <p>{transcript}</p>
         </div>
     );
